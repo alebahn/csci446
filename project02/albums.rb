02f5a5ac4fdb6d44c95dac35d36c613f5412d48c
Aaron Lebahn <cplusplus328@gmail.com>
2013-01-27 19:57:11 -0700

    Implemented detection of which page is requested.

--- project02/albums.rb
+++ project02/albums.rb
@@ -1,7 +1,13 @@
 require 'rack'
 
 hello_world = Proc.new do |env|
-  [200, {"Content-Type" => "text/plain"}, ["Hello from Rack!"]]
+  puts env
+  case env["REQUEST_PATH"]
+  when "/"
+    [200, {"Content-Type" => "text/plain"}, ["Hello from Rack!"]]
+  else
+    [404, {"Content-Type" => "text/plain"}, ["Page Not Found."]]
+  end
 end
 
 Rack::Handler::WEBrick.run hello_world, :Port => 8080
