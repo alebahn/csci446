fd0fc85a4a67d2ce6738812a294050752c45a180
Aaron Lebahn <cplusplus328@gmail.com>
2013-03-07 15:47:36 -0700

    Added a fixture and enabled it for the products unit tests.

--- project04/test/unit/product_test.rb
+++ project04/test/unit/product_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
   test "pruduct attributes must not be empty" do
     product = Product.new
     assert product.invalid?
